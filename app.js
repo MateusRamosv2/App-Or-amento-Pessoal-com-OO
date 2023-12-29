e26f9b514cbf8c0639078d2bb13ad96e24bebfa1
MateusRamos <mateusramosv@hotmail.com>
2023-12-29 17:03:32 -0300

    Listando despesas finalizado

--- app.js
+++ app.js
@@ -132,7 +132,53 @@ function carregaListaDespesas() {
 
     despesas = bd.recuperarTodosRegistros()
 
-    console.log(despesas)
+    let listaDespesas = document.getElementById('listaDespesas')
+
+
+    //<tr>
+      //  <td>15/03/2018</td>
+      //  <td>Alimentação</td>
+      //  <td>Compras do mÊs</td>
+      //  <td>477.45</td>
+    //</tr>*\
+
+    despesas.forEach(function(d) {
+
+        //console.log(d)
+        
+        //criando as linhas (tr)
+        let linha = listaDespesas.insertRow()
+
+
+        //criando as colunas (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        
+
+        switch(d.tipo) {
+            case '1' : d.tipo = 'Alimentação'
+                break
+          
+            case '2' : d.tipo = 'Educação'
+                break
+          
+            case '3' : d.tipo = 'Lazer'
+                break
+          
+            case '4' : d.tipo = 'Saúde'        
+                break
+            
+            case '5' : d.tipo = 'Transporte'        
+                break    
+        }   
+
+        linha.insertCell(1).innerHTML = d.tipo
+
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
+    
+
+
 }
 
 
